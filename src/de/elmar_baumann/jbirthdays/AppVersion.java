07b866929998405be4188c2245450bafe491d35c
ebaumann <ebaumann@localhost>
2014-01-10 21:43:20 +0100

    Added Version info

---
+++ src/de/elmar_baumann/jbirthdays/AppVersion.java
@@ -0,0 +1,12 @@
+package de.elmar_baumann.jbirthdays;
+
+/**
+ * @author Elmar Baumann
+ */
+public final class AppVersion {
+
+    public static final String VERSION = "0.1";
+
+    private AppVersion() {
+    }
+}
