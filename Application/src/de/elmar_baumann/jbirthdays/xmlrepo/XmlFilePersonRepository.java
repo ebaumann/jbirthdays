9e91b073b2aa3a4fa744c2e32b16d3f0d37a0e2f
ebaumann <ebaumann@localhost>
2014-01-16 10:17:10 +0100

    Database can be moved to different folders

--- Application/src/de/elmar_baumann/jbirthdays/xmlrepo/XmlFilePersonRepository.java
+++ Application/src/de/elmar_baumann/jbirthdays/xmlrepo/XmlFilePersonRepository.java
@@ -7,6 +7,12 @@ import de.elmar_baumann.jbirthdays.util.XmlUtil;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.prefs.Preferences;
 import org.bushe.swing.event.EventBus;
 
@@ -74,13 +80,14 @@ public final class XmlFilePersonRepository {
 
     synchronized File getFile() {
         return xmlFile;
-}
+    }
 
     void setFile(File file) {
         if (file == null) {
             throw new NullPointerException("file == null");
         }
         synchronized (this) {
+            Logger.getLogger(XmlFilePersonRepository.class.getName()).log(Level.INFO, "Switching from database file ''{0}'' to database file ''{1}''", new Object[]{xmlFile, file});
             xmlFile = file;
             persistFilename(file);
         }
@@ -94,5 +101,27 @@ public final class XmlFilePersonRepository {
     private static void persistFilename(File file) {
         Preferences prefs = Preferences.userNodeForPackage(XmlFilePersonRepository.class);
         prefs.put(KEY_FILENAME, file.getAbsolutePath());
+    }
+
+    synchronized void moveTo(File dir) throws IOException {
+        if (dir == null) {
+            throw new NullPointerException("dir == null");
+        }
+        if (!dir.isDirectory() || dir.equals(xmlFile.getParentFile())) {
+            return;
+        }
+        File newFile = getNewFile(dir);
+        Logger.getLogger(XmlFilePersonRepository.class.getName()).log(Level.INFO, "Moving database ''{0}'' to directory ''{1}''", new Object[]{xmlFile, dir});
+        Files.copy(xmlFile.toPath(), newFile.toPath());
+        xmlFile.delete();
+        setFile(newFile);
+    }
+
+    private synchronized File getNewFile(File dir) {
+        String name = xmlFile.getName();
+        File newFile = new File(dir.getAbsolutePath() + File.separator + name);
+        return newFile.exists()
+                ? new File(dir.getAbsolutePath() + File.separator + new SimpleDateFormat("-yyyy-MM-dd_HH-mm-ss").format(new Date()) + ".xml")
+                : newFile;
     }
 }
